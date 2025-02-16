a4e2ecbf51faf5bf3a1ea778adcd7a81365a6c8f
Yehor Heiko <yehorheiko.developer@gmail.com>
2025-02-16 18:58:32 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function filterMovies(movie, query) {
+  const des = [...movie];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  return des.filter(
+    e =>
+      e.title.toLowerCase().includes(query.toLowerCase()) ||
+      e.description.toLowerCase().includes(query.toLowerCase()),
+  );
+}
+
+export const App = () => {
+  const [sort, setSort] = useState('');
+  const filtrSevr = filterMovies(moviesFromServer, sort);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={e => setSort(e.target.value.trim())}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={filtrSevr} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
